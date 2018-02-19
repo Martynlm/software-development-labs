f444e8827c8f2085fcec3b2f3f1d07ad9ce366c0
Martynlm <hockr@mail.ru>
2018-02-19 21:10:20 +0600

    add lab2
    add lab2

---
+++ lab2/lab2/Determination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Determination
+    {
+        public int Redefinition(string primary)
+        {
+            int value=1;
+            if (!Int32.TryParse(primary, out int intnumber))
+            {
+                value = 2;
+             }
+            if (!Double.TryParse(primary, out double doublenumber))
+            {
+                value = 0;
+            }
+            return value;
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primarystring = Console.ReadLine();
+            Determination calculator = new Determination();
+            int resultValue = calculator.Redefinition(primarystring);
+            Console.WriteLine(resultValue);
+            Console.ReadLine();
+
+        }
+    }
+}
