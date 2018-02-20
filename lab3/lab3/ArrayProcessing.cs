954b11f6a6ce0542da70e4d9fb485094d283acda
Martynlm <hockr@mail.ru>
2018-02-20 13:02:13 +0600

    add lab3
    add lab3

---
+++ lab3/lab3/ArrayProcessing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class ArrayProcessing
+    {
+        //public int Count(int[] mas, int size)
+        //{
+           
+        //    return ;
+        //}
+
+        public List<int> Even(int[] mas, int size)
+        {
+            List<int> evenNumbers = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                if(mas[i]%2==0){
+                    evenNumbers.Add(mas[i]);
+                }
+            }
+            return evenNumbers;
+        }
+
+        //public int Uneven(int[] mas, int size)
+        //{
+        //    return;
+        //}
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Дан целочисленный массив размера N. Вывести вначале все его четные элементы, а затем — нечетные.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = string.Empty; //сюда мы сохраним введенную информацию 
+            s = Console.ReadLine();//непосредственно считывание. 
+            string[] st = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int size = st.Length;
+            int[] mas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = Convert.ToInt32(st[i]);
+            }
+            ArrayProcessing even = new ArrayProcessing();
+            List<int> evenNumbers = even.Even(mas, size);
+            Console.WriteLine(evenNumbers);
+            Console.ReadLine();
+        }
+    }
+}
