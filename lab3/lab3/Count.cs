af97aca5642ba56bc2e23610af0b841a60b860bb
Martynlm <hockr@mail.ru>
2018-02-21 20:41:02 +0600

    add lab3
    add lab3

---
+++ lab3/lab3/Count.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3
+{
+    class Count
+    {
+        private int[] array;
+        public Count(int[] beginArray)
+        {
+           array = beginArray;
+        }
+
+        public void Sorting()
+        {
+            List<int> evenNumbers = new List<int>();
+            List<int> unevenNumbers = new List<int>();
+            foreach (int i in array)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbers.Add(i);
+                }
+                else
+                {
+                    unevenNumbers.Add(i);
+                }
+            }
+            Console.Write("Четные: ");
+            foreach (int i in evenNumbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write("\nНечетные: ");
+            foreach (int i in unevenNumbers)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дан целочисленный массив размера N. Вывести вначале все его четные элементы, а затем — нечетные.
+using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();//объявление и считывание. 
+            string[] st = array.Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            int size = st.Length;
+            int[] mas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = Convert.ToInt32(st[i]);
+            }
+            Count evenNumbers = new Count(mas);
+            evenNumbers.Sorting();
+            Console.ReadLine();
+        }
+    }
+}
