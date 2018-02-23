00aeeff8056082e4db954b3bd8885e4c7800b208
Martynlm <hockr@mail.ru>
2018-02-23 16:21:49 +0600

    add lab4
    add lab4

---
+++ lab4/lab4/Permutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Permutation
+    {
+        private int[] arr;
+     public Permutation(int[] beginArray)
+        {
+            arr = beginArray;
+        }
+
+ 
+        public void Grouping()
+        {
+            Array.Reverse(arr);
+            int p = Array.IndexOf(arr, arr.Max());
+            int tempp = arr[p];
+            arr[p] = arr[9];
+            arr[9] = tempp;
+            int o = Array.IndexOf(arr, arr.Min());
+            int tempo = arr[o];
+            arr[o] = arr[0];
+            arr[0] = tempo;
+            foreach (int k in arr)
+            {
+                Console.WriteLine(k);
+            }
+        }
+
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Дан массив размера 10. Переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами.
+using System;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();//объявление и считывание. 
+            string[] st = array.Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            int[] mas = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = Convert.ToInt32(st[i]);
+            }
+            Permutation arrayNumbers = new Permutation(mas);
+            arrayNumbers.Grouping();
+            Console.ReadLine();
+        }
+    }
+}
