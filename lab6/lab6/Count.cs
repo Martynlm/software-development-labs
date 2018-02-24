c27390e1cb4d5d995fa14d4a2c4d5e12245dc4ff
Martynlm <hockr@mail.ru>
2018-02-24 15:18:51 +0600

    add lab6
    add lab6

---
+++ lab6/lab6/Count.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Count
+    {
+        private int[] array;
+        private int sizeArray;
+        public Count(int[] beginArray, int size)
+        {
+            array = beginArray;
+            sizeArray = size;
+        }
+        public void Deleting()
+        {
+            for (int i = 1; i < sizeArray; i++)
+            {
+                if(array[i]%array[0]==0)
+                {
+                    for(int j = i; j<array.Length-1; j++)
+                    {
+                        array[j] = array[j + 1];
+                    }
+                    sizeArray--;
+                }
+            }
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,28 @@
+﻿//Удалить элементы массива кратные его первому элементу(первыйэлемент при этом не рассматривать). Тестовый пример 2 3 4 5 -1 -2 -3-4. Результат: 2 3 5 -1 -3. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();//объявление и считывание. 
+            string[] st = array.Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            int size = st.Length;
+            int[] mas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = Convert.ToInt32(st[i]);
+            }
+            Count newArray = new Count(mas, size);
+            newArray.Deleting(); 
+            Console.ReadLine();
+        }
+    }
+}
