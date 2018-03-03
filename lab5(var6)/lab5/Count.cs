147b7526d73f2d46c8005171f1ed5b581cce3e4c
Martynlm <hockr@mail.ru>
2018-03-04 02:02:27 +0600

    add lab5(var6)

---
+++ lab5(var6)/lab5/Count.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Count
+    {
+        private int[] array;
+        public Count(int[] beginArray)
+        {
+            array = beginArray;
+        }
+
+        private int NumberOfPositive()
+        {
+            int value=0;
+            foreach(int i in array)
+            {
+                if (i > 0)
+                    value++;
+            }
+            return value;
+        }
+
+        private double Average()
+        {
+            int averageOfNumbers = 0;
+            int sumOfPositiveNumbers = 0;
+            foreach (int i in array)
+            {
+                if(i>0)
+                {
+                    sumOfPositiveNumbers += i;
+                }
+            }
+            averageOfNumbers = sumOfPositiveNumbers / NumberOfPositive();
+            return averageOfNumbers;
+        }
+
+
+
+        public List<int> Addition()
+        {
+        List<int> newArray = new List<int>();
+            foreach (int i in array)
+            {
+               newArray.Add(i);
+               if(Math.Abs(i) > Average())
+                {
+                    newArray.Insert(newArray.IndexOf(i) + 1, 0);
+                }
+            }
+        return newArray;
+        }
+       
+    }
+}   
---
+++ lab5(var6)/lab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();//объявление и считывание. 
+            string[] st = array.Split(new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries);
+            int size = st.Length;
+            int[] mas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = Convert.ToInt32(st[i]);
+            }
+            Count newArray = new Count(mas);
+            foreach (int i in newArray.Addition())
+                Console.Write(i + " ");
+            Console.ReadLine();
+        }
+    }
+}
