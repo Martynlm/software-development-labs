96db3f84fa377e3f8ca3fd0c7c5ac82d5011d091
Martynlm <hockr@mail.ru>
2018-03-06 12:17:45 +0600

    add lab6 with editing

---
+++ lab6/lab6/Remover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace lab6
+{
+    class Remover
+    {
+        private List<int> array;
+        public Remover(int[] beginArray)
+        {
+            array = beginArray.ToList();
+        }
+
+        public List<int> Delete()
+        {
+
+            for (int i = 1; i < array.Count; i++)
+            {
+                if (i % array[0] == 0)
+                {
+                    array.Remove(i);
+                    i--;
+                }
+            }
+            return array;
+        }
+
+    }
+}
