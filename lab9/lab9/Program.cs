0f8d1173e3b0186f436d207e91ec3120ad3339dc
Martynlm <hockr@mail.ru>
2018-03-11 20:44:13 +0600

    add lab9 with editing

---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 3] { { 1, 6, 2 }, { 4, 3, 5 }, { 9, 7, 8 } };
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                    Console.Write(array[i, j] + " ");
+                Console.Write(Environment.NewLine);
+            }
+            Console.WriteLine("------");
+           Sorting newArray = new Sorting(array);
+            int[,] SortingArray = newArray.SortingNewArray();
+            for (int i = 0; i < SortingArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < SortingArray.GetLength(1); j++)
+                    Console.Write(SortingArray[i, j] + " ");
+                    Console.Write(Environment.NewLine);
+            }
+            Console.ReadLine();
+        }
+
+       
+    }
+}
---
+++ lab9/lab9/Sorting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    class Sorting
+    {
+        private int[,] array;
+        public Sorting(int[,] beginArray)
+        {
+        array = beginArray;
+        }
+
+        private void Swap(int i, int j, int x, int y)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[x, y];
+            array[x, y] = temp;
+        }
+
+
+        public int[,] SortingNewArray()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int x = 0; x < array.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < array.GetLength(1); y++)
+                        {
+                            if (array[i, j] > array[x, y])
+                                Swap(i, j, x, y);
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
