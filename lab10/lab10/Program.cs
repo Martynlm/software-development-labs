0b9b0f92057ba934e42b2409841a91a62296ee5b
Martynlm <hockr@mail.ru>
2018-03-11 20:44:25 +0600

    add lab10 with editing

---
+++ lab10/lab10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string line;
+            StreamReader sr = new StreamReader("input.txt");
+            List<int> mas = new List<int>();
+            while ((line = sr.ReadLine()) != null)
+            {
+                mas.Add(Convert.ToInt32(line));
+            }
+            foreach (int i in mas)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Remover newArray = new Remover(mas);
+            StreamWriter sw = new StreamWriter("output.txt");
+            foreach (int i in newArray.Delete())
+            {
+                sw.Write(i + " ");
+            }
+            sw.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab10/lab10/Remover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace lab10
+{
+    class Remover
+    {
+        private List<int> array;
+        public Remover(List<int> beginArray)
+        {
+            array = beginArray;
+        }
+
+        public List<int> Delete()
+        {
+            for (int i = 1; i < array.Count; i++)
+            {
+                if (array[i] % array[0] == 0)
+                {
+                    array.Remove(array[i]);
+                }
+            }
+            return array;
+        }
+
+    }
+}
